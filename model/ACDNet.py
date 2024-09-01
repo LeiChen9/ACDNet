ba4f2b08ff9a78a8990cd50092c573ed0e475a42
LeiChen9 <chenlei9691@gmail.com>
2024-09-01 18:17:12 +0800

    init acd

---
+++ model/ACDNet.py
@@ -0,0 +1,10 @@
+'''
+Author: LeiChen9 chenlei9691@gmail.com
+Date: 2024-08-31 14:10:40
+LastEditors: LeiChen9 chenlei9691@gmail.com
+LastEditTime: 2024-09-01 17:49:51
+FilePath: /Poutry/Users/lei/Documents/Code/ACDNet/model/ACDNet.py
+Description: 
+
+Copyright (c) 2024 by Riceball, All Rights Reserved. 
+'''
